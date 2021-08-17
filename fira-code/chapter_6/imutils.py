e71108feb78e025ac3b4bf25481281aaa21d9153
Fraol-Gmichael <Fraol.Gmichael@gmail.com>
2021-08-17 14:04:51 -0700

    flipped

--- fira-code/chapter_6/imutils.py
+++ fira-code/chapter_6/imutils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+# translate means to shft the image to the left, right, top and bottom
 def translate(image, x, y):
     M = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
@@ -30,4 +31,7 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 
     resized = cv2.resize(image, dim, interpolation=inter)
     return resized
+
+
+
 
